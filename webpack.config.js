47c57a8fb0c7ab1f4e41bfd2bc212d1b9ac898af
Michael <ko4erov@mail.ru>
2019-11-28 21:05:12 +0300

    webpack ready and first code

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,34 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-// const CopyWebpackPlugin = require('copy-webpack-plugin')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = {
 	entry: {
+		// путь до входного файла от файла конфигурации webpack
 		app: './src/index.js'
 	},
 	output: {
 		// текущий [name] берётся из ярлыка entry (app), на выходе файл будет app.js
-		filename: '[name].js',
+		filename: 'assets/js/[name].js',
+		// как будет называться папка, где будет собираться проект
 		path: path.join(__dirname, '/dist'),
 		// publicPath нужен для корректной работы devServer
-		publicPath: '/dist'
+		publicPath: ''
 	},
 	module: {
 		rules: [{
 			test: /\.js$/,
 			loader: 'babel-loader',
 			exclude: '/node_modules/'
+		},{
+			test: /\.(png|jpg|jpeg|svg)$/,
+			loader: 'file-loader',
+			options: {
+				name: 'assets/img/[name].[ext]'
+			}
 		},{
 			test: /\.scss$/,
 			use: [
@@ -61,10 +70,24 @@ module.exports = {
 		overlay: true
 	},
 	plugins: [
+		// если не заданы параметры, то удаляет все файлы из path: path.join(__dirname, '/dist')
+		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin({
 			// [name] ссылается на ярлык в entry (app)
-			filename: "[name].css"
+			filename: "assets/css/[name].css"
 		}),
+		new HtmlWebpackPlugin({
+			// пока отключил
+			hash: false,
+			// откуда брать html
+			template: path.join(__dirname, './src/html/index.html'),
+			// куда скопировать
+			filename: './index.html'
+		}),
+		new CopyWebpackPlugin([
+			{ from: path.join(__dirname, './src/img'), to: 'assets/img'},
+			{ from: path.join(__dirname, './static'), to: ''}
+		]),
 		new webpack.SourceMapDevToolPlugin({
 			filename: '[file].map'
 		})
